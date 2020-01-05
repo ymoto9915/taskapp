b2c8001b9e224641de549b6b8e5beda2d81f4224
mikan <mikan927ty@gmail.com>
2020-01-05 15:57:03 +0900

    タスクコントローラー作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "新しいタスク[#{task.name}]を登録したよ"
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
