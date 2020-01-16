4d29404b33e88c62b651e60a1eeeaa06674ef1e8
mikan <mikan927ty@gmail.com>
2020-01-16 19:59:12 +0900

    名前がない場合『名無しさん」と自動でつけるコールバック

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
@@ -7,5 +9,9 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名無しさん' if name.blank?
   end
 end
